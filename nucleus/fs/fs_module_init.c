5f4f192faf0411fbfb269bad8583722fa9719fc4
bosley <josh.bosley@proton.me>
2025-12-28 21:08:17 -0500

    cruftbgone

--- nucleus/fs/fs_module_init.c
+++
@@ -1,8 +0,0 @@
-#include "fs_module.h"
-
-akx_cell_t *fs_module_init_impl(akx_runtime_ctx_t *rt, akx_cell_t *args) {
-  fs_ensure_init(rt);
-  akx_cell_t *result = akx_rt_alloc_cell(rt, AKX_TYPE_SYMBOL);
-  akx_rt_set_symbol(rt, result, "fs-initialized");
-  return result;
-}
